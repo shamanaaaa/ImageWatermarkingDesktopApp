93fe703a4e215256260a35a742893f870d6e1a74
shamanaaaa <STart000>
2021-08-27 20:18:26 +0200

    modify for real use with one big X letter overlay

--- convert.py
+++ convert.py
@@ -8,8 +8,9 @@ def image_converter(image, dst_folder, count, size):
     txt = Image.new('RGBA', img.size, (255, 255, 255, 0))
 
     # Creating Text
-    text = "Mária Domanová\nneupravené zábery"
-    font = ImageFont.truetype("arial.ttf", size)
+    # text = "Mária Domanová\nneupravené zábery"
+    text = "X"
+    font = ImageFont.truetype("ubuntu.ttf", size)
 
     # Creating Draw Object
     draw = ImageDraw.Draw(txt)
@@ -18,11 +19,19 @@ def image_converter(image, dst_folder, count, size):
     width, height = img.size
 
     # Loop for Multiple Watermarks
-    y_var = height / count
-    for i in range(count):
-        x = (width / 2 - 200)
-        y = y_var * i + 50
-        draw.text((x, y), text, fill=(255, 255, 255, 35), font=font)
+    # y_var = height / count
+    # for i in range(count):
+    #     x = (width / 2 - 200)
+    #     y = y_var * i + 50
+    #     draw.text((x, y), text, fill=(255, 255, 255, 35), font=font)
+    if height == 900:
+        x = -20
+        y = -80
+    else:
+        x = 120
+        y = -210
+
+    draw.text((x, y), text, fill=(255, 255, 255, 85), font=font)
 
     # Combining both layers and saving new image
     watermarked = Image.alpha_composite(img, txt)
